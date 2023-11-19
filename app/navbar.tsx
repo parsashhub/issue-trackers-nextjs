7b8ff4f3f2a600fc5be6b6783bdac7babf656106
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-20 03:28:22 +0330

    build navbar

---
+++ app/navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+
+const Navbar = () => {
+  const links = [
+    {
+      name: "Dashboard",
+      href: "/",
+    },
+    { name: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          >
+            {link.name}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
