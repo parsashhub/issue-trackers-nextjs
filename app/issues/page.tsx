84d228c3536fb48cbfdc9826f241d6a736f99f3e
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-20 04:31:07 +0330

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@radix-ui/themes";
+
 const IssuesPage = () => {
-    return <div>issues</div>
+  return (
+    <div>
+      issues
+      <Button>New Issue</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/app/navbar";
+import { Theme } from "@radix-ui/themes";
+import "./globals.css";
+import "@radix-ui/themes/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
+        <Theme>
+          <Navbar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
