ca94890b571d81b7383c08a7a2f6a62c2718a776
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-24 00:24:26 +0330

    add edit button to issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,7 +1,9 @@
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import StatusBadge from "@/app/components/statusBadge";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface Props {
   params: { id: string };
@@ -15,16 +17,24 @@ const IssieDetail = async ({ params }: Props) => {
   if (!issue) return notFound();
 
   return (
-    <>
-      <Heading>{issue.title}</Heading>
-      <Flex className="space-x-3 my-2">
-        <StatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose max-w-full mt-4">
-        <Text>{issue.description}</Text>
-      </Card>
-    </>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex className="space-x-3 my-2">
+          <StatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose max-w-full mt-4">
+          <Text>{issue.description}</Text>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
