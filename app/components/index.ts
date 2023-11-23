6ad398898cd7879d58caee3664bc3691d16914b1
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-24 00:16:26 +0330

    add an index.ts in components folder to organize import statements

---
+++ app/components/index.ts
@@ -0,0 +1,5 @@
+export { default as CustomLink } from "./customLink";
+export { default as ErrorMessage } from "./errorMessage";
+export { default as StatusBadge } from "./statusBadge";
+export { default as Spinner } from "./spinner";
+export { default as Skeleton } from "./skeleton";
