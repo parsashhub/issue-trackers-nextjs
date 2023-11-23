02a483c568f51a0a270e5b309cf61958afeabd41
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-23 23:55:17 +0330

    style the issue detail component page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,20 +1,31 @@
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import StatusBadge from "@/app/components/statusBadge";
 
 interface Props {
   params: { id: string };
 }
 
 const IssieDetail = async ({ params }: Props) => {
-  if (typeof params.id !== "number") return notFound();
-
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
 
   if (!issue) return notFound();
 
-  return <div></div>;
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <Flex className="space-x-3 my-2">
+        <StatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose max-w-full" mt="4">
+        <Text>{issue.description}</Text>
+      </Card>
+    </>
+  );
 };
 
 export default IssieDetail;
