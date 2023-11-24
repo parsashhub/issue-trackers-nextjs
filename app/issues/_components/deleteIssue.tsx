814b830572b1d0c60d95bb1c5fae4be30c5a84b9
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-25 00:18:54 +0330

    implement the functionality if deleting issue in deleteIssue.tsx

--- app/issues/_components/deleteIssue.tsx
+++ app/issues/_components/deleteIssue.tsx
@@ -1,16 +1,34 @@
 "use client";
 import {
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent, AlertDialogDescription,
-    AlertDialogRoot,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-    Button, Flex,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogRoot,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Button,
+  Flex,
 } from "@radix-ui/themes";
 import { TrashIcon } from "@radix-ui/react-icons";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const DeleteIssue = ({ issueId }: { issueId: string }) => {
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/api/issues/${issueId}`);
+      router.push("/issues/list");
+      router.refresh();
+      toast.success("issue deleted successfully");
+    } catch (e: any) {
+      toast.error(e.message);
+    }
+  };
+
   return (
     <AlertDialogRoot>
       <AlertDialogTrigger>
@@ -21,17 +39,19 @@ const DeleteIssue = ({ issueId }: { issueId: string }) => {
       </AlertDialogTrigger>
       <AlertDialogContent>
         <AlertDialogTitle>Confirm Deletion</AlertDialogTitle>
-          <AlertDialogDescription>
-              Are you sure? This action can not be undone.
-          </AlertDialogDescription>
-          <Flex className="mt-4" gap="3">
-              <AlertDialogCancel>
-                  <Button color="gray">Cancel</Button>
-              </AlertDialogCancel>
-              <AlertDialogAction>
-                  <Button color="red"> Delete</Button>
-              </AlertDialogAction>
-          </Flex>
+        <AlertDialogDescription>
+          Are you sure? This action can not be undone.
+        </AlertDialogDescription>
+        <Flex className="mt-4" gap="3">
+          <AlertDialogCancel>
+            <Button color="gray">Cancel</Button>
+          </AlertDialogCancel>
+          <AlertDialogAction>
+            <Button onClick={handleDelete} color="red">
+              Delete
+            </Button>
+          </AlertDialogAction>
+        </Flex>
       </AlertDialogContent>
     </AlertDialogRoot>
   );
